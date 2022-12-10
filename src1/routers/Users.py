1d5e0c2852217ca3f382a76554917ce302fc3549
Inf3rn01 <115873101+Inf3rn01@users.noreply.github.com>
2022-12-10 11:25:34 +0300

    upd

---
+++ src1/routers/Users.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+from sql_base.models import User
+from resolvers import check_login_1
+
+user_router = APIRouter()
+
+
+@user_router.get('/')
+def not_login():
+    return {"Message": "Login in system"}
+
+
+@user_router.get('/login')
+def check_login(user: User):
+    position_id = check_login_1(user)
+    if position_id:
+        return {"code": 200, "message": "Login correct", "position_id": position_id}
+    else:
+        return {"code": 401, "message": "Login incorrect, try again", "position_id": None}
---
+++ src1/sql_base/__init__.py
@@ -0,0 +1,6 @@
+from base_gangs import BaseGangs
+import models
+from setting import BASE_PATH
+
+base_gangs = BaseGangs()
+base_gangs.set_base_path(BASE_PATH)
